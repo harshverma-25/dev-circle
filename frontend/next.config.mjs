0da7b2f1940a4d5fb8dd43581e8bc9941773a6a0
Harsh Verma <hv.harshverma025@gmail.com>
2026-05-01 10:22:33 +0530

    fix cors problem in google oauth

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -23,6 +23,10 @@ const nextConfig = {
             key: 'Cross-Origin-Opener-Policy',
             value: 'same-origin-allow-popups', // Required for Google OAuth
           },
+          {
+            key: 'Referrer-Policy',
+            value: 'no-referrer-when-downgrade', // Required to retain origin on redirection
+          },
         ],
       },
     ];
